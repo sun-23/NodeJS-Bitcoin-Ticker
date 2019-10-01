15baefac2403a008aab4aa0541e5045ce04548a2
Chaichana <49665324+sun-23@users.noreply.github.com>
2019-10-01 16:09:26 +0700

    add amount
    1 install nodejs 
    2 create folder Bitcoin-Ticker then put this 2 file to it 
    3 command line cd Bitcoin-Ticker npm init then npm install express request nodemon body-parser
    4 run with nodemon index.js or node index.js
    
    /*-----------------*/
    if it cannot install npm and error Unhandled rejection Error : EACCES : permission denined ...... or Other.
    use this command  sudo npm cache clean --force --unsafe --perm 
    then it will Ask the password 
    Enter password 
    see more https://npm.community/t/unhandled-rejection-error-eacces-permission-denied-angular-npm-install/4981
    
    if port cannot use 
    command lsof -i TCP:(port number) | grep LISTEN (example lsof -i TCP:3000 | grep LISTEN)
    then it will show node 1111 ....  (this is example)
    command kill -9 1111 (this is example)
    see more https://www.murarinayak.com/blog/technology/how-to-resolve-eaddrinuse-address-already-in-use-error/

--- index.js
+++ index.js
@@ -3,44 +3,55 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // GET method route
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 // POST method route
-app.post("/",function(req,res){
+app.post("/", function(req, res) {
   console.log(req.body.Crypto);
   console.log(req.body.Money);
-
- // get value form index.html
+  console.log(req.body.amount);
+  // get value form index.html
   var crypto = req.body.Crypto;
-  var money = req.body.Money;
+  var fiat = req.body.Money;
+  var amount = req.body.amount;
 
-// set urlAPi
-  var urlAPi = "https://apiv2.bitcoinaverage.com/indices/global/ticker/" + crypto + money ;
-  console.log(urlAPi);
+  // set options
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+
   // request API error,response,body
- request(urlAPi, function(error,response,body){
- // ParseJSON
-   var data = JSON.parse(body);
-   var price = data.last;
-   var date = data.display_timestamp;
+  request(options, function(error, response, body) {
+    // ParseJSON
+    var data = JSON.parse(body);
+    var price = data.price;
+    var date = data.time;
 
-   console.log(price);
-   // show result
-   res.write("<p>The current date is " + date + "</p>");
-   res.write("<h1>The price of " + crypto + " is " + price + " " + money + "</h1>");
-   // end
-   res.send();
+    // console.log(price);
+    // show result
+    res.write("<p>The current date is " + date + "</p>");
+    res.write("<h1>The price of " + amount + " " + crypto + " is " + price + " " + fiat + "</h1>");
+    // end
+    res.send();
 
- });
+  });
 
 });
 
 // start server
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Server running ");
 });
